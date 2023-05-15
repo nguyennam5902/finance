0816b1743eb571cae6c8bb31b28ea1893cc70dad
Nguyễn Hải Nam 20205104 <83709960+nguyennam5902@users.noreply.github.com>
2023-05-15 04:12:47 +0000

    TODO:  Add account in /regiter

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 // Build Node
 const express = require('express'), fs = require('fs'), app = express();
 const ejs = require('ejs');
+const res = require('express/lib/response');
 const { MongoClient } = require('mongodb');
 const client = new MongoClient('mongodb+srv://test:test@database.uzhnq7w.mongodb.net/');
 client.connect();
+function isBlank(str) {
+    return (!str || str.trim().length === 0);
+}
 /**
  * Connect to collection 'accounts' inside database 'finance' with username and password
+ * @param username
+ * @param password
+ * @returns bool: True if the account exists and otherwise
  */
-async function run(username, password) {
+async function checkAccount(username, password) {
     try {
         const collection = client.db('finance').collection('accounts');
         const aggregation = [{ $match: { 'username': username, 'password': password } }, { $count: 'result' }];
         const a = collection.aggregate(aggregation).toArray();
-        console.log((await a).length);
+        const length = (await a).length;
+        return (length != 0);
     } catch (err) {
         console.error(err);
     }
 }
-
-
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
@@ -47,29 +53,67 @@ app.get('/login', function (req, res) {
 app.get('/layout', (_req, res) => {
     res.render('layout');
 });
+app.get('apology', (_req, res) => {
+    res.render('apology');
+});
+function escape(s) {
+    const replacements = [["-", "--"], [" ", "-"], ["_", "__"], ["?", "~q"], ["%", "~p"], ["#", "~h"], ["/", "~s"], ["\"", "''"]];
+    for (var i = 0; i < replacements.length; i++)
+        s = s.replace(replacements[i][0], replacements[i][1]);
+    return s;
+}
+
+function apologyRender(res, top, bottom) {
+    bottom = escape(bottom);
+    res.render('apology', {
+        main: '<img alt="' + top + '" class="border" src="http://memegen.link/custom/' + top + '/' + bottom + '.jpg?alt=https://i.imgur.com/CsCgN7Ll.png" title=' + bottom + '>',
+        top: top, bottom: bottom
+    });
+}
+
 app.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    console.log(`username: ${username}, password: ${password}`);
-    // here you can save the data to a database or perform any other necessary actions
-    run(username, password).catch(console.error());
-    res.send('Login successful');
+    if (isBlank(username))
+        apologyRender(res, 403, 'must provide username');
+    else if (isBlank(password))
+        apologyRender(res, 403, 'must provide password');
+    // console.log(`username: ${username}, password: ${password}`);
+    checkAccount(username, password).then(isValid => {
+        if (isValid == true) {
+            // res.send('Succeed');
+        } else {
+            apologyRender(res, 403, 'invalid username and/or password');
+        }
+    });
 });
 
 app.get('/', (_req, res) => {
-    res.render(__dirname + '/templates/login.ejs');
+    res.render('login');
 });
 app.get('/register', (_req, res) => {
-    res.sendFile(__dirname + '/templates/register.html');
+    res.render('register');
 });
 app.post('/register', (req, res) => {
-    console.log("Register");
     const username = req.body.username;
     const password = req.body.password;
     const confirmation = req.body.confirmation;
+    if (isBlank(username)) {
+        apologyRender(res, 400, 'must provide username');
+    } else if (isBlank(password)) {
+        apologyRender(res, 400, 'must provide password');
+    } else if (password !== confirmation) {
+        apologyRender(res, 400, 'Password does not match');
+    }
     console.log(`username: ${username}, password: ${password}, confirmation: ${confirmation}`);
     // here you can save the data to a database or perform any other necessary actions
-    res.send('Login successful');
+    checkAccount(username, password).then(isValid => {
+        if (isValid == true) {
+            apologyRender(res, 400, 'The username existed, choose a other one');
+        } else {
+            res.send('Register successful');
+        }
+    });
 });
 // Run
 app.listen(app.get('port'), () => {
